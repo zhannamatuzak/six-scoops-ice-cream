169333386bca9056fb30521f47a21f10af3f43f7
zhannamatuzak <volokhovazhanna@gmail.com>
2023-09-15 09:24:50 +0200

    Add rules content

--- intro.py
+++ intro.py
@@ -11,9 +11,34 @@ logo = """
   \___ \   | |    > <    \___ \ | |    | |  | || |  | ||  ___/  \___ \ 
   ____) | _| |_  / . \   ____) || |____| |__| || |__| || |      ____) |
  |_____/ |_____|/_/ \_\ |_____/  \_____|\____/  \____/ |_|     |_____/
-"""                                     
+"""
 
+RULES = """
+Begin by selecting your skill level. 
 
+The word you have to guess will get longer 
+as your skill level increases.
+
+Easy Level: Short words (3 letters) for beginners. 
+Medium Level: 4-letter words for those seeking a challenge.
+Hard Level: 5-letter challenging words for experts.
+
+Then try and guess the word one letter at a time 
+before you lose all six scoops of ice cream.
+
+Here is a hint.
+All words are related to summer.
+
+You will start off with 6 scoops of ice cream. 
+
+For each wrong guess you lose one scoop.
+And you can play until you stay only with the cane.
+It is sad 😢
+
+Cheer up! You can try again 😉, 
+simply restart the game.
+Just press the RUN PROGRAM at the top.
+"""
 
 ice_cream_scoops = {
     0: """
--- run.py
+++ run.py
@@ -3,4 +3,6 @@ import random  # generates random numbers
 import string  # module for string (ascii, emojis) manipulation
 from time import sleep  # time delay for print statements
 from words import words  # imports word list from word.py
+import colorama
 from colorama import Fore, Back, Style, init  # adds color to terminal text
+
