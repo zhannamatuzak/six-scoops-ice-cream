063b48d35f3cb49e7f38e7e147b6d463eedfa4ad
zhannamatuzak <volokhovazhanna@gmail.com>
2023-09-16 16:11:49 +0200

    Fix bug and add flowchart image

--- run.py
+++ run.py
@@ -14,34 +14,35 @@ def choose_level(level):
     """
     Chooses level based on user input
     """
-    if level == 1:
+    if level == "1":
         print(
             f"{Style.NORMAL}{Fore.LIGHTBLUE_EX}Easy:",
             f"{Style.RESET_ALL}Get your 🍦 easily cause it is 🔥.\n"
         )
         return "easy"
-    elif level == 2:
+    elif level == "2":
         print(
             f"{Fore.LIGHTYELLOW_EX}Medium:",
             f"{Style.RESET_ALL}Get your 🍦 smartly cause it is 🔥.\n"
         )
-        return "medium" 
-    elif level == 3:
+        return "medium"
+    elif level == "3":
         print(
             f"{Fore.LIGHTRED_EX}Hard:",
             f"{Style.RESET_ALL}Strain your brain to get 🍦 cause it is 🔥.\n"
         )
         return "hard"
-    
-    
+
+
 def validate_level(value):
     """
     Checks if user input for level choice equals 1, 2 or 3
     """
     try:
-        if (value != 1) and (value != 2) and (value != 3):
+        if (value != "1") and (value != "2") and (value != "3"):
             raise ValueError(
-                f"Please only enter 1, 2 or 3. You typed this "
+                f"Please only enter 1, 2 or 3. You typed "
+                f"{Style.BRIGHT}{Fore.YELLOW}{value}{Style.RESET_ALL}"
             )
     except ValueError as e:
         print(f"{Fore.RED}Invalid data: {e}, please try again\n")
@@ -55,7 +56,7 @@ def user_level():
     """
     while True:
         input_text = f"{Fore.GREEN}Choose your level:\n\n 1. Easy\n 2. Medium\n 3. Hard\n {Style.RESET_ALL}"
-        chosen_level = int(input(input_text))
+        chosen_level = input(input_text)
         level = choose_level(chosen_level)
 
         if validate_level(chosen_level):
@@ -80,14 +81,15 @@ def select_words(words, level):
     elif level == "Hard":
         hard = [word for word in words if len(word) < 6]
         return hard
-    
+
 
 def game_rules():
     """
     Function to ask the player if he wants to read the game rules
     """
     while True:
-        print(f"Do you want to read the game {Style.BRIGHT}{Fore.YELLOW}rules?(Y/N)")
+        print(
+            f"Do you want to read the game {Style.BRIGHT}{Fore.YELLOW}rules?(Y/N)")
         answer = input().upper().strip()
         if answer == "Y":
             print(intro.RULES)
@@ -105,23 +107,25 @@ def run_intro():
     """
     print(f"{Style.BRIGHT}{Fore.RED}{intro.LOGO}")
     print("Welcome!")
-    print(f"Here you can get the Extra Large treat\nwith Six {Fore.LIGHTYELLOW_EX}Scoops of Ice Cream.\n")
+    print(
+        f"Here you can get the Extra Large treat\nwith Six {Fore.LIGHTYELLOW_EX}Scoops of Ice Cream.\n")
     print("🍦🍦🍦🍦🍦🍦\n")
     sleep(1)
     print("Oh, it is not that easy. But yummy! 🤤\n")
     sleep(0.5)
     print(
         f"Here are the rules on how to get your {Style.BRIGHT}Extra Large treat!\n")
-    
+
 
 def main():
     """
     Runs entire application 
     """
+    global word_list
     run_intro()
     game_rules()
     word_list = user_level()
-
+    
 
 if __name__ == "__main__":
     main()
