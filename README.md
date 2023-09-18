19f0936ac366c0d97d702066876b23d9eafca52f
zhannamatuzak <volokhovazhanna@gmail.com>
2023-09-18 19:41:31 +0200

    Create two functions to start the game and to decremet scoops for wrong guess

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Six Scoops
-Six Scoops is a terminal-based alternative to a hangman game for kids. This game combines learning with fun. The player tries to guess a word letter by letter before running out of six scoops of ice cream. It is told the player that it is very hot and it is a good idea to get an ice cream. Playing this game is an effective way to encourage kids' interest in learning English. While playing this game, kids will learn easy vocabulary about summer. The words contain 3 to 5 letters and are appropriate for young learners.
+Six Scoops is a terminal-based alternative to a hangman game for kids. This game combines learning with fun. The player tries to guess a word letter by letter before running out of six scoops of ice cream. It is told the player that it is very hot and it is a good idea to get an ice cream. Playing this game is an effective way to encourage kids' interest in learning English. While playing this game, kids will learn easy vocabulary (100 words) about summer. The words contain 3 to 5 letters and are appropriate for young learners.
 
 [Link to live site]()
 
@@ -22,6 +22,11 @@ The following flowchart is created with [lucidchart](www.lucidchart.com). It vis
 
 ## Bugs
 
++ **Solved bugs**
+
+1. The ````word_completion``` string looked like one line. And when the guessed letters joined it, letters sticked to each other. The text readability was very poor. So the user may have a bad experience.
+
+  - *Solutions:* added spaces between each underscore character
 ## Testing
 
 ## Deployment
--- run.py
+++ run.py
@@ -10,7 +10,10 @@ from colorama import Fore, Back, Style, init  # adds color to terminal text
 
 init(autoreset=True)  # sets text to its default values
 
+
+alphabet = set(string.ascii_uppercase)  # stores letters A-Z
 scoops = 5
+guessed_letters = []  # stores letters guessed by user
 
 
 def choose_level(level):
@@ -86,13 +89,113 @@ def select_words(words, level):
         return hard
 
 
-def get_valid_word(words):
+def get_word():
     """
     Chooses randomly a word from the chosen words list
     and returns it in uppercase
     """
     word = random.choice(words)
     return word.upper()
+
+
+def validate_letter(letter):
+    """
+    Validates user input if letter is in alphabet
+    """
+    try:
+        if letter not in alphabet:
+            raise ValueError(
+                f"Please enter a single letter from A to Z. "
+                f"You typed {Fore.YELLOW}{letter}{Style.RESET_ALL}"
+            )
+    except ValueError as e:
+        print(f"{Fore.RED}Invalid data: {e}{Fore.RESET}.\n")
+        return False
+    return True
+
+
+def start_game(word):
+    """
+    Runs and updates game development, provides 
+    feedback to the user until the game is over.
+    """
+    # variables
+    underscored_word = "_ " * len(word)
+    word_completion = underscored_word[:-1]
+    guessed = False
+    guessed_letters = []
+    guessed_words = []  # number ice cream scoops that can be lost
+    print("Let's play!")
+    # prints a number of scoops and displays the word to guess
+    print(intro.display_scoops(scoops))
+    print(f"{Fore.LIGHTBLUE_EX}{word_completion}")
+    print("\n")
+    # a while loop to handle the users input
+    while not guessed and scoops > 0:
+        guess = users_letter()
+        # checks if a guessed letter has already been guessed or belongs to the word
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You have already guessed the letter", guess)
+            elif guess not in word:
+                scoops_number()
+                guessed_letters.append(guess)
+            else:
+                print("Good job!,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(
+                    word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        #
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You have already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not in the word.")
+                scoops_number()
+                guessed_words.append(guess)
+            else:
+                guessed: True
+                word_completion = word
+        else:
+            print("It is not a valid guess.")
+        print(intro.display_scoops(scoops))
+        print(f"{Fore.LIGHTBLUE_EX}{word_completion}")
+        print("\n")
+    # shows the end of the game: user wins or loses
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you have only a cone left 😢 the word was " +
+              word + ". Maybe next time!")
+
+
+def users_letter():
+    """
+    Asks a user to type a letter and checks if it is in the word
+    """
+    while True:
+        guess = input("Please guess a letter:\n").upper()
+        if validate_letter(guess):
+            break
+    return guess
+
+
+def scoops_number():
+    """
+    Decrements scoops for each wrong guess
+    """
+    global guess
+    global guessed_letters
+    global scoops
+    scoops -= 1
+    print(
+        f"Wrong letter! You have {Style.BRIGHT}{Fore.YELLOW}{scoops}{Style.RESET_ALL} scoops of 🍦 left.\n")
 
 
 def game_rules():
@@ -146,9 +249,9 @@ def main():
     Runs entire application 
     """
     global word_list
-    run_intro()
-    game_rules()
     word_list = user_level()
+    word = get_word()
+    start_game(word)
 
 
 if __name__ == "__main__":
